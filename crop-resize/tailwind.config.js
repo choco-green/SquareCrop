f807e792c9fd1c9fb6ab02da214aa577c1b280c9
justin <choco_green@hotmail.com>
2022-01-11 22:04:07 +0000

    TS for crop image

---
+++ crop-resize/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/*.html"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
